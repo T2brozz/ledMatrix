187717af01cea0b9f6f276219972e4de93656be1
t2brozz <42860763+T2brozz@users.noreply.github.com>
2023-02-09 19:21:56 +0100

    fix rowsetter

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn main() {
         parallel: 1 ,
         panel_type: None ,
         multiplexing: None,
-        row_setter:   String::from("ABCShiftRegisterRowAddressSetter")
+        row_setter:   String::from("SM5266RowAddressSetter")
     };
     let rows = config.rows as isize;
     let cols = config.cols as isize;
