6f5da82b92bcae97100ee4407ea15daa15bb9885
t2brozz <42860763+T2brozz@users.noreply.github.com>
2023-02-09 19:06:58 +0100

    fix rowsetter

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn main() {
         parallel: 1 ,
         panel_type: None ,
         multiplexing: None,
-        row_setter: Some(  String::from("DirectRowAddressSetter"))
+        row_setter:   String::from("DirectRowAddressSetter")
     };
     let rows = config.rows as isize;
     let cols = config.cols as isize;
