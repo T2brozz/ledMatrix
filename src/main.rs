55f11939fceb47d2014518a9cda28115037137d9
t2brozz <42860763+T2brozz@users.noreply.github.com>
2023-02-12 22:30:26 +0100

    fix string

--- src/main.rs
+++ src/main.rs
@@ -65,8 +65,9 @@ fn main() {
     loop{
         let time_now = Utc::now();
         let (hours,minutes,seconds)= (time_now.hour(),time_now.minute(), time_now.second());
+        let time_str=format!("{}\n{}\n{}", hours,minutes,seconds).as_str();
         let text = Text::new(
-            format!("{}\n{}\n{}", hours,minutes,seconds).as_str(),
+            time_str,
             Point::new((cols / 2) as i32, (rows / 2) as i32),
             text_style
         );
