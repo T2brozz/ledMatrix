db63aa0165910b6b06ae245a9941706012f2383b
t2brozz <42860763+T2brozz@users.noreply.github.com>
2023-02-12 22:06:27 +0100

    try with text

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@ use crate::calender::get_calender;
 use crate::weather::{get_weather, ParseWeatherError};
 use rpi_led_panel::{RGBMatrix, RGBMatrixConfig};
 use tokio::time::{sleep,Duration};
-
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::Rgb888,
+    prelude::*,
+    primitives::{Circle, Line, PrimitiveStyle},
+    text::{Alignment, Text},
+    Drawable,
+};
 mod weather;
 mod calender;
 mod secrets;
@@ -51,45 +58,15 @@ fn main() {
     let cols = config.cols as isize;
     let (mut matrix, mut canvas) = RGBMatrix::new(config, 0).expect("Matrix initialization failed");
 
-    let [center_x, center_y] = [cols / 2, rows / 2];
-
-    let rotate_square = (rows.min(cols) as f64 * 1.41) as isize;
-    let min_rotate = center_x - rotate_square / 2;
-    let max_rotate = center_x + rotate_square / 2;
-
-    let display_square = (rows.min(cols) as f64 * 0.7) as isize;
-    let min_display = center_x - display_square / 2;
-    let max_display = center_x + display_square / 2;
-
-    for step in 0.. {
-        let rotation_deg = step as f64 / 2.0;
-        for x in min_rotate..max_rotate {
-            for y in min_rotate..max_rotate {
-                let [rot_x, rot_y] =
-                    rotate([x - center_x, y - center_x], rotation_deg.to_radians());
-                let canvas_x = rot_x + center_x as f64;
-                let canvas_y = rot_y + center_y as f64;
-                if (min_display..max_display).contains(&x)
-                    && (min_display..max_display).contains(&y)
-                {
-                    canvas.set_pixel(
-                        canvas_x as usize,
-                        canvas_y as usize,
-                        scale_col(x, min_display, max_display),
-                        255 - scale_col(y, min_display, max_display),
-                        scale_col(y, min_display, max_display),
-                    )
-                } else {
-                    canvas.set_pixel(canvas_x as usize, canvas_y as usize, 0, 0, 0)
-                }
-            }
-        }
-
+    let text = Text::with_alignment(
+        "Hello\nWorld",
+        Point::new(cols / 2, rows / 2),
+        MonoTextStyle::new(&FONT_6X10, Rgb888::WHITE),
+        Alignment::Center,
+    );
+    loop{
+        text.draw(canvas.as_mut()).unwrap();
         canvas = matrix.update_on_vsync(canvas);
 
-        if step % 120 == 0 {
-            print!("\r{:>100}\rFramerate: {}", "", matrix.get_framerate());
-            std::io::stdout().flush().unwrap();
-        }
     }
 }
