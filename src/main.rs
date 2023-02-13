ef38dae2f524618b68ed66767316f14205336d20
t2brozz <42860763+T2brozz@users.noreply.github.com>
2023-02-13 22:05:30 +0100

    image stuff

--- src/main.rs
+++ src/main.rs
@@ -93,7 +93,7 @@ async fn main() {
         );
         //temperature.draw(canvas.as_mut()).unwrap();
         let newiamge=last_response.icon_img.resize_exact(12,12,FilterType::Gaussian);
-        let image_data = ImageRawBE::<Rgb888>::new(newiamge.as_bytes(), 11 as u32);
+        let image_data = ImageRawBE::<Rgb888>::new(newiamge.as_bytes(), 15 as u32);
         let image= Image::new(
             &image_data,
             Point::new(10,10)
