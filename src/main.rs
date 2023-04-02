1dd378e22640d70e7b2d76300524c073135384c0
t2brozz <42860763+T2brozz@users.noreply.github.com>
2023-04-02 21:28:12 +0200

    fix times

--- src/main.rs
+++ src/main.rs
@@ -61,12 +61,11 @@ async fn main() {
          get_calender().await.expect("First try to get calender events failed")
         );
     let mut current_event = CurrentEvent { text_scroll: 5.0, event_index: 0 };
-    let on_off_times= (DateTime::parse_from_rfc2822("Tue, 1 Jul 2003 07:00:00 +0100").unwrap().time() , DateTime::parse_from_rfc2822("Tue, 1 Jul 2003 23:00:00 +0100").unwrap().time());
+    let on_off_times= (DateTime::parse_from_rfc2822("Tue, 1 Jul 2003 07:00:00 +0000").unwrap().time() , DateTime::parse_from_rfc2822("Tue, 1 Jul 2003 22:00:00 +0000").unwrap().time());
     let mut display_on=true;
     loop {
 
         let time_now = Utc::now();
-        println!("{:?}",(on_off_times,time_now.time()));
         if time_now.time()>= on_off_times.0 && time_now.time() <= on_off_times.1{
             display_on=true;
         }else {
