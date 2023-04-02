fc5211b5c94454026683ab964e375d11f3a07cf1
t2brozz <42860763+T2brozz@users.noreply.github.com>
2023-04-02 21:20:39 +0200

    fix times

--- src/main.rs
+++ src/main.rs
@@ -64,11 +64,7 @@ async fn main() {
     let on_off_times= (DateTime::parse_from_rfc2822("Tue, 1 Jul 2003 07:00:00 +0100").unwrap().timestamp() , DateTime::parse_from_rfc2822("Tue, 1 Jul 2003 23:00:00 +0100").unwrap().timestamp());
     let mut display_on=true;
     loop {
-
-        if !display_on {
-            continue
-        }
-        canvas.fill(0, 0, 0);
+        println!("{}",display_on);
 
         let time_now = Utc::now();
         if time_now.timestamp()>= on_off_times.0 && time_now.timestamp() <= on_off_times.1{
@@ -76,6 +72,11 @@ async fn main() {
         }else if time_now.timestamp()>=on_off_times.1 {
             display_on=false;
         }
+        if !display_on {
+            continue
+        }
+        canvas.fill(0, 0, 0);
+
 
         let time_str = time_now.with_timezone(&Berlin).format("%H\n%M\n%S").to_string();
         let clock = Text::new(
