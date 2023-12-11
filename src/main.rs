16b0c6b4f1cb0e3581e73ec9fd03a76979f27226
t2brozz <42860763+T2brozz@users.noreply.github.com>
2023-12-11 21:51:21 +0100

    add img

--- src/main.rs
+++ src/main.rs
@@ -67,7 +67,7 @@ async fn main() {
     let image_data = ImageRawBE::<Rgb888>::new(new_image.as_bytes(), 1_u32);
     let image = Image::new(
         &image_data,
-        Point::new(10, 10),
+        Point::new(20_i32, 30_i32),
     );
     loop {
         let time_now = Utc::now();
